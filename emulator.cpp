213a4d56ffdad4c60749fe31e9bf65b859650ad3
mojtaba <mojtaba@DESKTOP-57M28DK>
2025-05-31 12:33:30 +0330

    Add project files.

---
+++ emulator.cpp
@@ -0,0 +1,12 @@
+﻿// emulator.cpp : Defines the entry point for the application.
+//
+
+#include "emulator.h"
+#include <LIEF/LIEF.hpp>
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
